da20e52f1d835e75545d3d7767816cc69a36e145
thejasondev <jason.guerra1002@gmail.com>
2025-03-07 12:13:13 -0500

    middlewares modified

--- miramar-shop-backend/config/middlewares.ts
+++ miramar-shop-backend/config/middlewares.ts
@@ -1,11 +1,34 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '256mb', // Aumentar límite
+      jsonLimit: '256mb', // Aumentar límite
+      textLimit: '256mb', // Aumentar límite
+      formidable: {
+        maxFileSize: 200 * 1024 * 1024, // Aumentar a 200mb
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
-  'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
