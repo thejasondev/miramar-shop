8dd79cbc13f103fdf50b961633dea5ff8fe2c68c
thejasondev <jason.guerra1002@gmail.com>
2025-04-14 12:34:53 -0500

    eliminated miramar-shop-backend

--- miramar-shop-backend/config/database.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: { rejectUnauthorized: false },
-    },
-  },
-}); 
